054bd9f470564ed8071b0f729678cc8ab21cdd7b
jbastidas-sbs <87920077+jbastidas-sbs@users.noreply.github.com>
2023-01-20 19:04:53 -0400

    Integración para API rest con clientes

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -8,6 +8,7 @@ use App\Models\Customer;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CustomerController extends Controller
 {
@@ -93,5 +94,57 @@ class CustomerController extends Controller
         $customer->restore();
 
         return Redirect::back()->with('success', 'Cliente restaurado.');
+    }
+    // Api
+    public function indexApi()
+    {
+        $customers = Customer::orderByName()->get();
+        return response()->json(['customers' => $customers]);
+    }
+
+    public function storeApi(array $input)
+    {
+        Validator::make($input, [
+            'name' => ['required', 'max:50'],
+            'fiscal_identification' => ['required', 'max:9'],
+            'telephone' => ['nullable', 'max:50'],
+            'email' => ['nullable', 'max:50', 'email'],
+            'address' => ['nullable', 'max:150'],
+        ])->validate();
+        Customer::create([
+                'name' => $input['name'],
+                'fiscal_identification' => $input['fiscal_identification'],
+                'telephone' => $input['telephone'],
+                'email' => $input['email'],
+                'address' => $input['address'],
+        ]);
+        return response()->json(['success' => 'Cliente registrado.']);
+    }
+
+    public function updateApi(array $input)
+    {
+        Validator::make($input, [
+            'name' => ['required', 'max:50'],
+            'fiscal_identification' => ['required', 'max:9'],
+            'telephone' => ['nullable', 'max:50'],
+            'email' => ['nullable', 'max:50', 'email'],
+            'address' => ['nullable', 'max:150'],
+        ])->validate();
+        $customer = Customer::find($input['id']);
+        $customer->update([
+                'name' => $input['name'],
+                'fiscal_identification' => $input['fiscal_identification'],
+                'telephone' => $input['telephone'],
+                'email' => $input['email'],
+                'address' => $input['address'],
+            ]);
+        return response()->json(['success' => 'Cliente actualizado.']);
+    }
+
+    public function destroyApi(array $input)
+    {
+        $customer = Customer::find($input['id']);
+        $customer->delete();
+        return response()->json(['success' => 'Cliente borrado.']);
     }
 }
